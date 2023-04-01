f243091471ead6e7ac38b3c4da9ca250da561a69
Valeriy V. Vorotyntsev <valery.vv@gmail.com>
2023-04-01 19:15:17 +0300

    Enable `easymotion` plugin

--- init.lua
+++ init.lua
@@ -66,6 +66,11 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: First, some plugins that don't require any configuration
 
+  -- REVIEW: If you notice that easymotion misbehaves (e.g. conflicts
+  -- with LSP), consider giving 'ggandor/lightspeed' or 'phaazon/hop'
+  -- a try.
+  'easymotion/vim-easymotion',
+
   -- Git related plugins
   'tpope/vim-fugitive',
   'tpope/vim-rhubarb',
@@ -276,7 +281,8 @@ pcall(require('telescope').load_extension, 'fzf')
 
 -- See `:help telescope.builtin`
 vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+-- XXX-HACK: '<leader><space>' == '<space><space>' conflicts with easymotion
+-- vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
 vim.keymap.set('n', '<leader>/', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
   require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
