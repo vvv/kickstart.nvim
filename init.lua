ac0d0b9e0461f20950ffd19acccc0b8029945e87
Valeriy V. Vorotyntsev <valery.vv@gmail.com>
2026-04-03 20:44:35 +0300

    init.lua: Enable `clangd` and `pyright` LSP servers

--- init.lua
+++ init.lua
@@ -628,9 +628,9 @@ require('lazy').setup({
       --  See `:help lsp-config` for information about keys and how to configure
       ---@type table<string, vim.lsp.Config>
       local servers = {
-        -- clangd = {},
+        clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
         -- rust_analyzer = {},
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
